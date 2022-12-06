5c91a02cc398362133e1bd5146218f7fdfcc3fa3
Ed Singleton <singletoned@gmail.com>
2022-12-06 21:33:45 +0000

    Add download page to urls

--- business_case_approval/urls.py
+++ business_case_approval/urls.py
@@ -6,6 +6,7 @@ from business_case_approval.application import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("application/<int:application_id>/", views.page_view, name="pages-index"),
+    path("application/<int:application_id>/download", views.page_view, name="download-pdf"),
     path("application/<int:application_id>/<str:page_name>", views.page_view, name="pages"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
